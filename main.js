132a3c9dcb5d2b04a42093f2ad532f7f2564a7bd
Rodolfo Noguez <rodolfonoguez10@gmail.com>
2025-06-21 21:30:21 -0500

    new update

--- main.js
+++ main.js
@@ -62,7 +62,8 @@ class Tree{
                 node.data = minNode.data;
                 node.right = this.deleteRec(node.right, minNode.data);
             }
-    }
+        }
+    }  
 
     findMin(node){
         while(node.left !== null){
@@ -72,8 +73,89 @@ class Tree{
     }
 
     find(value){
-
+        return this.findRec(this.root, value);
     }
+
+    findRec(node,value){
+        if(node === null || node.data === value){return node;}
+
+        if(value < node.data){
+            return this.findRec(node.left,value);
+        }else if(value > node.data){
+            return this.findRec(node.right,value);
+        }
+    }
+
+    levelOrder(callback){
+        if(!callback){return;}
+
+        const queue = [];
+        queue.push(this.root);
+
+        while(queue.length > 0){
+            const currentNode = queue.shift();
+            if(callback){callback(currentNode);}
+            if(currentNode.left !== null){queue.push(currentNode.left);}
+            if(currentNode.right !== null){queue.push(currentNode.right);}
+        }
+    }
+
+    inOrder(callback){
+        if(!callback){return;}
+        
+        const inOrderRec = (node) => {
+            if(node === null){return;}
+            inOrderRec(node.left);
+            callback(node);
+            inOrderRec(node.right);
+        };
+
+        inOrderRec(this.root);
+    }
+
+    preOrder(callback){
+        if(!callback){return;}
+
+        const preOrderRec = (node) => {
+            if(node === null){return;}
+            callback(node);
+            preOrderRec(node.left);
+            preOrderRec(node.right);
+        };
+
+        preOrderRec(this.root);
+    }
+
+    postOrder(callback){
+        if(!callback){return;}
+
+        const postOrderRec = (node) => {
+            if(node === null){return;}
+            postOrderRec(node.left);
+            postOrderRec(node.right);
+            callback(node);
+        };
+
+        postOrderRec(this.root);
+    }
+
+    height(value){
+        return heightRec(this.root, value, 0);
+    }
+    
+    heightRec(node, value, height){
+        if(node === null){return -1;}
+
+        if(value < node.data){
+            return this.heightRec(node.left, value, height + 1);
+        }else if(value > node.data){
+            return this.heightRec(node.right, value, height + 1);
+        }else{
+            return height;
+        }
+    }
+
+
 
 
 }
